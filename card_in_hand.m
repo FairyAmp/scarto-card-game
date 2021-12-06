55e9bc7978767dde8dbf57ee2bb809d0c129414e
Amp <amppan@ucdavis.edu>
2021-12-05 21:43:29 -0800

    check if card is in hand

---
+++ card_in_hand.m
@@ -0,0 +1,9 @@
+function yes_in_hand = card_in_hand(card, hand)
+    for i = 1:length(hand)
+        if cardEquals(card, hand(i))
+            yes_in_hand = 1;
+            return
+        end
+    end
+    yes_in_hand = false;
+end
